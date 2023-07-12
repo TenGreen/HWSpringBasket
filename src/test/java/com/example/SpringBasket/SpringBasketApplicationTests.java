221d1baad21c568b718c1825795b83a414b8c1b7
TenGreen <Grishintut@yandex.ru>
2023-07-12 14:05:32 +0300

    initialise commit

---
+++ src/test/java/com/example/SpringBasket/SpringBasketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBasket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
